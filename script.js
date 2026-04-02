8d574fc129f624d910bdf2c48dde67c13314821a
b2030 <basil9774@gmail.com>
2026-04-02 16:02:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById('orderForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('شكراً لك! تم استلام طلبك، وسيقوم فريقنا (الوسيط) بالتواصل معك لضمان أفضل سعر وأعلى جودة.');
+    this.reset();
+});
